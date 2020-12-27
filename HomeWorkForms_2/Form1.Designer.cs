c011d6d02333689805cf8f76a74a52e595b2fe08
Максим Богащенко <pl_a_st@live.ru>
2020-12-27 18:14:03 +0300

    27/12/20//18/13

--- HomeWorkForms_2/Form1.Designer.cs
+++ HomeWorkForms_2/Form1.Designer.cs
@@ -28,28 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.GenerateBT = new System.Windows.Forms.Button();
+            this.moreOrLessBt = new System.Windows.Forms.Button();
             this.resultLbl = new System.Windows.Forms.Label();
+            this.digitsGB = new System.Windows.Forms.GroupBox();
+            this.unitsRB = new System.Windows.Forms.RadioButton();
+            this.tensRB = new System.Windows.Forms.RadioButton();
+            this.hundredsRB = new System.Windows.Forms.RadioButton();
+            this.thousandsRB = new System.Windows.Forms.RadioButton();
+            this.moreOrLessChB = new System.Windows.Forms.CheckBox();
+            this.digitsGB.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // GenerateBT
             // 
-            this.button1.Location = new System.Drawing.Point(12, 34);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.GenerateBT.Location = new System.Drawing.Point(12, 34);
+            this.GenerateBT.Name = "GenerateBT";
+            this.GenerateBT.Size = new System.Drawing.Size(100, 23);
+            this.GenerateBT.TabIndex = 0;
+            this.GenerateBT.Text = "Сгенерировать";
+            this.GenerateBT.UseVisualStyleBackColor = true;
+            this.GenerateBT.Click += new System.EventHandler(this.GenerateBT_Click);
             // 
-            // button2
+            // moreOrLessBt
             // 
-            this.button2.Location = new System.Drawing.Point(12, 63);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.moreOrLessBt.Location = new System.Drawing.Point(12, 63);
+            this.moreOrLessBt.Name = "moreOrLessBt";
+            this.moreOrLessBt.Size = new System.Drawing.Size(100, 23);
+            this.moreOrLessBt.TabIndex = 1;
+            this.moreOrLessBt.Text = "Увеличить";
+            this.moreOrLessBt.UseVisualStyleBackColor = true;
+            this.moreOrLessBt.Click += new System.EventHandler(this.moreOrLessBt_Click);
             // 
             // resultLbl
             // 
@@ -60,16 +69,93 @@
             this.resultLbl.TabIndex = 2;
             this.resultLbl.Text = "Результат:  9999";
             // 
+            // digitsGB
+            // 
+            this.digitsGB.Controls.Add(this.thousandsRB);
+            this.digitsGB.Controls.Add(this.hundredsRB);
+            this.digitsGB.Controls.Add(this.tensRB);
+            this.digitsGB.Controls.Add(this.unitsRB);
+            this.digitsGB.Location = new System.Drawing.Point(118, 25);
+            this.digitsGB.Name = "digitsGB";
+            this.digitsGB.Size = new System.Drawing.Size(152, 68);
+            this.digitsGB.TabIndex = 3;
+            this.digitsGB.TabStop = false;
+            this.digitsGB.Text = "Разряды";
+            // 
+            // unitsRB
+            // 
+            this.unitsRB.AutoSize = true;
+            this.unitsRB.Checked = true;
+            this.unitsRB.Location = new System.Drawing.Point(7, 20);
+            this.unitsRB.Name = "unitsRB";
+            this.unitsRB.Size = new System.Drawing.Size(70, 17);
+            this.unitsRB.TabIndex = 0;
+            this.unitsRB.TabStop = true;
+            this.unitsRB.Text = "Единицы";
+            this.unitsRB.UseVisualStyleBackColor = true;
+            // 
+            // tensRB
+            // 
+            this.tensRB.AutoSize = true;
+            this.tensRB.Location = new System.Drawing.Point(7, 43);
+            this.tensRB.Name = "tensRB";
+            this.tensRB.Size = new System.Drawing.Size(69, 17);
+            this.tensRB.TabIndex = 1;
+            this.tensRB.TabStop = true;
+            this.tensRB.Text = "Десятки";
+            this.tensRB.UseVisualStyleBackColor = true;
+            // 
+            // hundredsRB
+            // 
+            this.hundredsRB.AutoSize = true;
+            this.hundredsRB.Location = new System.Drawing.Point(83, 20);
+            this.hundredsRB.Name = "hundredsRB";
+            this.hundredsRB.Size = new System.Drawing.Size(55, 17);
+            this.hundredsRB.TabIndex = 2;
+            this.hundredsRB.TabStop = true;
+            this.hundredsRB.Text = "Сотни";
+            this.hundredsRB.UseVisualStyleBackColor = true;
+            // 
+            // thousandsRB
+            // 
+            this.thousandsRB.AutoSize = true;
+            this.thousandsRB.Location = new System.Drawing.Point(83, 44);
+            this.thousandsRB.Name = "thousandsRB";
+            this.thousandsRB.Size = new System.Drawing.Size(63, 17);
+            this.thousandsRB.TabIndex = 3;
+            this.thousandsRB.TabStop = true;
+            this.thousandsRB.Text = "Тысячи";
+            this.thousandsRB.UseVisualStyleBackColor = true;
+            // 
+            // moreOrLessChB
+            // 
+            this.moreOrLessChB.AutoSize = true;
+            this.moreOrLessChB.Checked = true;
+            this.moreOrLessChB.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.moreOrLessChB.Location = new System.Drawing.Point(15, 102);
+            this.moreOrLessChB.Name = "moreOrLessChB";
+            this.moreOrLessChB.Size = new System.Drawing.Size(144, 17);
+            this.moreOrLessChB.TabIndex = 4;
+            this.moreOrLessChB.Text = "Увеличить/Уменьшить";
+            this.moreOrLessChB.UseVisualStyleBackColor = true;
+            this.moreOrLessChB.CheckedChanged += new System.EventHandler(this.moreOrLessChB_CheckedChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(300, 130);
+            this.Controls.Add(this.moreOrLessChB);
+            this.Controls.Add(this.digitsGB);
             this.Controls.Add(this.resultLbl);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.moreOrLessBt);
+            this.Controls.Add(this.GenerateBT);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HomeWorkForms_2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.digitsGB.ResumeLayout(false);
+            this.digitsGB.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -77,9 +163,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button GenerateBT;
+        private System.Windows.Forms.Button moreOrLessBt;
         private System.Windows.Forms.Label resultLbl;
+        private System.Windows.Forms.GroupBox digitsGB;
+        private System.Windows.Forms.RadioButton thousandsRB;
+        private System.Windows.Forms.RadioButton hundredsRB;
+        private System.Windows.Forms.RadioButton tensRB;
+        private System.Windows.Forms.RadioButton unitsRB;
+        private System.Windows.Forms.CheckBox moreOrLessChB;
     }
 }
 
--- HomeWorkForms_2/Form1.cs
+++ HomeWorkForms_2/Form1.cs
@@ -16,5 +16,82 @@ namespace HomeWorkForms_2
         {
             InitializeComponent();
         }
+        /// <summary>
+        ////Результат
+        /// </summary>
+        public double Result
+        { get; private set;}
+        /// <summary>
+        ///Вывести результат 
+        /// </summary>
+        private void PrintResult()
+        {
+            resultLbl.Text = string.Format("Результат: {0}", Result);
+        }
+        /// <summary>
+        ////Переиминовать кнопку Увеличит_Уменьшить в зависимости от чекбокса
+        /// </summary>
+        private void renameCheckMoreOrLessBt()
+        {
+            if (moreOrLessChB.Checked)
+            {
+                moreOrLessBt.Text = "Увеличить";
+            }
+            else
+            {
+                moreOrLessBt.Text = "Уменьшить";
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (Control control in Controls)
+            {
+                if (control.Name != "GenerateBT")
+                {
+                    control.Enabled = false;
+                }
+            }
+            renameCheckMoreOrLessBt();
+        }
+        /// <summary>
+        /// Сгенерировать число от 0 до 9999
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GenerateBT_Click(object sender, EventArgs e)
+        {
+            const int MIN_RANDOM_NUMBER = 0;
+            const int MAX_RANDOM_NUMBER = 9999;
+            foreach (Control control in Controls)
+            {
+                control.Enabled = true;
+            }
+            Random rnd = new Random();
+            Result = rnd.Next(MIN_RANDOM_NUMBER,MAX_RANDOM_NUMBER);
+            PrintResult();
+        }
+        private void moreOrLessChB_CheckedChanged(object sender, EventArgs e)
+        {
+            renameCheckMoreOrLessBt();
+        }
+
+        private void moreOrLessBt_Click(object sender, EventArgs e)
+        {
+            if(moreOrLessChB.Checked)
+            {
+                if (unitsRB.Checked) { Result++; }
+                if (tensRB.Checked) { Result = Result + 10;}
+                if (hundredsRB.Checked) { Result = Result + 100; }
+                if (thousandsRB.Checked) { Result=Result + 1000; }
+            }
+            else
+            {
+                if (unitsRB.Checked) { Result--; }
+                if (tensRB.Checked) { Result = Result - 10; }
+                if (hundredsRB.Checked) { Result = Result - 100; }
+                if (thousandsRB.Checked) { Result = Result - 1000; }
+            }
+            PrintResult();
+        }
     }
 }
